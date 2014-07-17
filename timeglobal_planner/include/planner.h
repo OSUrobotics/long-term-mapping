1d3a27ba74c87f197b5a73ef92e6bbadb65db4bc
Cameron Bowie <bowiec@onid.orst.edu>
2014-07-16 17:21:12 -0700

    modified smoothness tolerance

--- timeglobal_planner/include/planner.h
+++ timeglobal_planner/include/planner.h
@@ -2,13 +2,17 @@
 #define PLANNER_H
 
 
-#define NORM_STEP    2
-#define DIAG_STEP    3
-#define TIME_STEP    5
+#define NORM_STEP     2
+#define DIAG_STEP     3
+#define TIME_STEP     5
 
-#define LETHAL_COST  100
-#define OPEN_COST    0
-#define UNKNOWN_COST -1
+#define LETHAL_COST   100
+#define OPEN_COST     0
+#define UNKNOWN_COST  -1
+
+#define SMOOTH_TOL    0.0000001
+#define DATA_WEIGHT   0.5
+#define SMOOTH_WEIGHT 0.5
 
 
 #endif
